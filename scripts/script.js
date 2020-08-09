e8140d752dd4aba0aa569386895c86812df1bf37
wilsondomingos <wilson.md@hotmail.com>
2020-08-09 16:34:34 +0100

    Nova Atualizacao

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,7 @@ function toggleHeader() {
     if (window.pageYOffset > 60 && $header.classList.contains('max-header')) {
         $header.classList.remove('max-header');
         $header.classList.add('min-header');
-        $logo.classList.remove('max-logo');
-        $logo.classList.add('min-logo');
+     
         $navBar.classList.remove('max-nav');
         $navBar.classList.add('min-nav');
         $menu.firstElementChild.classList.remove('max-hamburger');
@@ -16,8 +15,7 @@ function toggleHeader() {
     } else if (window.pageYOffset <= 60 && $header.classList.contains('min-header')) {
         $header.classList.add('max-header');
         $header.classList.remove('min-header');
-        $logo.classList.add('max-logo');
-        $logo.classList.remove('min-logo');
+      
         $navBar.classList.add('max-nav');
         $navBar.classList.remove('min-nav');
         $menu.firstElementChild.classList.add('max-hamburger');
